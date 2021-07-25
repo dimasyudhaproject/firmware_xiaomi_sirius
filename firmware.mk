20fdac8a907c23ee27d7f120c94c6951fb3b0659
Dimas Yudha Pratama <dimas.yudha@next-ti.co.id>
2021-07-25 22:19:27 +0700

    sirius: Initial push of Sirius firmware
    Based on MIUI V12.5.1.0.QEBCNXM
    
    Signed-off-by: Dimas Yudha Pratama <dimas.yudha@next-ti.co.id>

---
+++ firmware.mk
@@ -0,0 +1,20 @@
+PRODUCT_COPY_FILES += \
+	firmware/xiaomi/sirius/firmware/abl.elf:install/firmware-update/abl.elf \
+    firmware/xiaomi/sirius/firmware/aop.mbn:install/firmware-update/aop.mbn \
+    firmware/xiaomi/sirius/firmware/BTFM.bin:install/firmware-update/BTFM.bin \
+    firmware/xiaomi/sirius/firmware/cmnlib.mbn:install/firmware-update/cmnlib.mbn \
+    firmware/xiaomi/sirius/firmware/cmnlib64.mbn:install/firmware-update/cmnlib64.mbn \
+    firmware/xiaomi/sirius/firmware/devcfg.mbn:install/firmware-update/devcfg.mbn \
+    firmware/xiaomi/sirius/firmware/dspso.bin:install/firmware-update/dspso.bin \
+    firmware/xiaomi/sirius/firmware/dtbo.img:install/firmware-update/dtbo.img \
+    firmware/xiaomi/sirius/firmware/hyp.mbn:install/firmware-update/hyp.mbn \
+    firmware/xiaomi/sirius/firmware/keymaster64.mbn:install/firmware-update/keymaster64.mbn \
+    firmware/xiaomi/sirius/firmware/logfs_ufs_8mb.bin:install/firmware-update/logfs_ufs_8mb.bin \
+    firmware/xiaomi/sirius/firmware/logo.img:install/firmware-update/logo.img \
+    firmware/xiaomi/sirius/firmware/NON-HLOS.bin:install/firmware-update/NON-HLOS.bin \
+    firmware/xiaomi/sirius/firmware/qupv3fw.elf:install/firmware-update/qupv3fw.elf \
+    firmware/xiaomi/sirius/firmware/storsec.mbn:install/firmware-update/storsec.mbn \
+    firmware/xiaomi/sirius/firmware/tz.mbn:install/firmware-update/tz.mbn \
+	firmware/xiaomi/sirius/firmware/vbmeta.img:install/firmware-update/vbmeta.img \
+    firmware/xiaomi/sirius/firmware/xbl.elf:install/firmware-update/xbl.elf \
+    firmware/xiaomi/sirius/firmware/xbl_config.elf:install/firmware-update/xbl_config.elf
